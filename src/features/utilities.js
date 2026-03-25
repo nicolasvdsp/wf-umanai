803ee3850e7e279aed1227e0c290eb465bc37f66
Nicolas van der Straten Ponthoz <nicolasvds@live.be>
2026-03-25 10:32:31 +0100

    refactor(utilities): strips dynamic year function to the minimum required

--- src/features/utilities.js
+++ src/features/utilities.js
@@ -1,18 +1,11 @@
 function initDynamicCurrentYear(container) {
   container = container || document;
+
   const currentYear = new Date().getFullYear();
-  const yearInBinary = currentYear.toString(2);
-  const yearInBase10 = currentYear.toString(10);
-  const paddedYearForScramble = yearInBase10.padStart(yearInBinary.length, '_');
-
   const currentYearElements = container.querySelectorAll('[data-current-year]');
-  if (!currentYearElements.length) return;
   currentYearElements.forEach(currentYearElement => {
-    currentYearElement.textContent = yearInBinary;
-    currentYearElement.setAttribute('data-scramble-text', paddedYearForScramble);
+    currentYearElement.textContent = currentYear;
   });
-
-
 }
 
 
